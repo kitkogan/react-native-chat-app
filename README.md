5d62f92cb8e3f012e560476ee63ad83e1e23e9ca
Kit Kogan <kkogan.dev@gmail.com>
2020-03-15 19:45:10 -0500

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # react-native-chat-app
+
+I created this simple chat application using React Native and Socket.io. Requires Expo to spin up.
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,28 +1,24 @@
-const io = require('socket.io')();
-const messageHandler = require('./handlers/message.handler');
-
+const io = require("socket.io")();
+const messageHandler = require("./handlers/message.handler");
 
 let currentUserId = 2;
 const users = {};
 
 function createUserAvatarUrl() {
-    const rand1 = Math.round(Math.random() * 200 + 100);
-    const rand2 = Math.round(Math.random() * 200 + 100);
-    return `https://placeimg.com/${rand1}/${rand2}/any`
+  const rand1 = Math.round(Math.random() * 200 + 100);
+  const rand2 = Math.round(Math.random() * 200 + 100);
+  return `https://placeimg.com/${rand1}/${rand2}/any`;
 }
-//when server is connected
-//this function will listen for a message event
-//unser id will be created and incrememnted
-//messagehandler gets info form the message.handler component
-io.on('connection', socket => {
-    console.log('a user connected');
-    console.log(socket.id);
-    users[socket.id] = {userId: currentUserId++};
-    socket.on('join', username => {
-        users[socket.id].username = username;
-        users[socket.io].avatar = createUserAvatarUrl();
-        messageHandler.handleMessage(socket, users);
-    }); 
+
+io.on("connection", socket => {
+  console.log("a user connected!");
+  console.log(socket.id);
+  users[socket.id] = { userId: currentUserId++ };
+  socket.on("join", username => {
+    users[socket.id].username = username;
+    users[socket.id].avatar = createUserAvatarUrl();
+    messageHandler.handleMessage(socket, users);
+  });
 });
 
 io.listen(3001);
